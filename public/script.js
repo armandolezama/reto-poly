894d252c0cccf96ab0d8ad7bc931682effb76a05
Armando Lezama <armando.lezama.psic@gmail.com>
2019-10-19 13:50:45 -0500

    se preparó la estructura para fetch

--- public/script.js
+++ public/script.js
@@ -1,6 +1,12 @@
-mainSection =  document.querySelector('#main-section')
-let limit = 20
+//Este código tiene como objetivo presentar una simulación de televisiones para web.
+//Cada televisión cuenta con botones para controlarse y un sintonizador manual de canales
+//El sintonizador (control) sirve para ir a un canal en específico, configurado para respetar los límites de la tv
 
+let limit = 20 //Esta línea se utiliza para especificar el límite de canales para cada televisión
+
+//Se considera que la siguiente clase cumple con el concepto de abstracción de POO ya que, en un sólo modelo
+//engloba diversas funciones repetibles de un televisor. Como siguiente paso cada clase debería tener contenida en sí misma
+//el código en html que le corresponda a cada instancia de objetos
 class Television {
     constructor(model, manualCh, channel = 1){
         this.model = document.getElementById(model)
@@ -9,9 +15,17 @@ class Television {
         this.channel = channel
     }
     theChannel(){
-        this.model.innerHTML = `<div class="screen"><p class="ch-content">${this.channel}</p></div>`
+        this.model.innerHTML = `
+        <div class="screen">
+            <p class="ch-content">${this.channel}</p>
+        </div>`
     }
     
+    // async tuneIn(){
+    //     await fetch().then().catch()
+        
+    //     return img
+    // }
     manualChannel(){
         if(this.turned){
             this.channel = this.manualCh.value
@@ -56,10 +70,12 @@ class Television {
     }
 }
 
+//Al poder ser utilizada repetidas veces cumple con el concepto de polimorfismo de POO
+//yaa que cada objeto instanciado con esta clase posee sus propios atributos, sólo se ingresa
+//el id en html del televisor, y el id de su control, y automáticamente se accede a sus métodos
+//y propiedades
 let firstTV = new Television ('first-tv', 'first-manual')
-
 let secondTV = new Television ('second-tv', 'second-manual')
-
 let thirdTV = new Television ('third-tv', 'third-manual')
 
 
