484d60a2d191537780c660236fa4adb44dea2efe
Armando Lezama <armando.lezama.psic@gmail.com>
2019-10-19 13:22:50 -0500

    Se han añadido 3 teles más, comentarios y modificaciones a la clase de tal manera que ahora permite encender, apagar y sintonizar manualmente

--- public/script.js
+++ public/script.js
@@ -1,31 +1,66 @@
-
+mainSection =  document.querySelector('#main-section')
 let limit = 20
 
 class Television {
-    constructor(model, channel = 1){
-        this.model = model
+    constructor(model, manualCh, channel = 1){
+        this.model = document.getElementById(model)
+        this.manualCh = document.getElementById(manualCh)
+        this.turned = false
         this.channel = channel
     }
     theChannel(){
-        this.model.innerHTML = `<div class="screen"><p>${this.channel}</p></div>`
+        this.model.innerHTML = `<div class="screen"><p class="ch-content">${this.channel}</p></div>`
     }
     
-    plusChannel(){
-        this.channel += 1
-        if(this.channel > limit ){
-            this.channel = 1
+    manualChannel(){
+        if(this.turned){
+            this.channel = this.manualCh.value
+            if(this.channel > limit ){
+                this.channel = limit
+            } else if (this.channel < 1){
+                this.channel = 1
+            }        
+            this.theChannel()
         }
-        this.theChannel()
+    }
+
+    plusChannel(){
+        console.log(this.turned)
+        if(this.turned){
+            this.channel += 1
+            if(this.channel > limit ){
+                this.channel = 1
+            }
+            this.theChannel()
+        }
     }
 
     lessChannel(){
-        this.channel -= 1
-        if(this.channel < 1){
-            this.channel = limit
+        if(this.turned){
+            this.channel -= 1
+            if(this.channel < 1){
+                this.channel = limit
+            }
+            this.theChannel()
         }
+    }
+
+    turnOff(){
+        this.turned = false
+        this.model.innerHTML = `<div class="screen"><p class="ch-content">Apagado</p></div>`
+    }
+
+    turnOn(){
+        this.turned = true
         this.theChannel()
     }
 }
 
-let firstTV = new Television (document.querySelector('.television'))
+let firstTV = new Television ('first-tv', 'first-manual')
+
+let secondTV = new Television ('second-tv', 'second-manual')
+
+let thirdTV = new Television ('third-tv', 'third-manual')
+
+
 
